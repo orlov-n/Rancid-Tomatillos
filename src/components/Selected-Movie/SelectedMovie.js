96682004c6698771d3f68fcffeccd548c1b6d40a
Nick Orlov <ialwaysreply@gmail.com>
2022-05-14 16:51:04 -0700

    Add background image

--- src/components/Selected-Movie/SelectedMovie.js
+++ src/components/Selected-Movie/SelectedMovie.js
@@ -3,9 +3,24 @@ import MovieCard from '../Movie-Card/Movie-Card'
 
 const SelectedMovie = ({selectedMovie}) => {
   return (
-    <div>
-      <img src={selectedMovie.poster_path}></img>
-    </div>
+    <main className="selected-movie-page" style={{
+      backgroundImage: `url(${selectedMovie.backdrop_path})`
+    }}>
+      <section>
+
+        <div>
+          <img src={selectedMovie.poster_path}></img>
+        </div>
+        <div>
+          <ul>
+            <li>{selectedMovie.title}</li>
+            <li>{selectedMovie.average_rating}</li>
+            <li>{selectedMovie.release_date}</li>
+          </ul>
+        </div>
+      </section>
+
+    </main>
   )
 }
 
